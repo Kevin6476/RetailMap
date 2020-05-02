a12d3f176dec19941930d96820517a17bc09f815
Kevin Picoli <kevinps6476@gmail.com>
2020-05-02 17:29:41 -0300

    Developer - Subindo exemplo de codigo no Angular

--- public/js/controllers/controller.main.js
+++ public/js/controllers/controller.main.js
@@ -1,5 +1,13 @@
 angular.module('retailmap')
 	.controller('controllerMain', ['$scope','$routeParams', function($scope, $routeParams) {
+
+		$scope.teste1 = 'ID';
+		$scope.teste2 = 'PLaceHolder';
+		$scope.teste3 = 'search';
+		$scope.teste4 = 'TesteLabel';
+		debugger;
+
+
 		$scope.testeClick = function() {
 			alert('Funcionou');
 		}
--- public/js/directives/diretrivas.js
+++ public/js/directives/diretrivas.js
@@ -1,2 +1,17 @@
-angular.module('retailmapDirectives', []);
-    //.directive('', function() {	});
+angular.module('retailmapDirectives', [])
+    .directive('formField', function() {
+       return {
+            restrict: 'E',
+            templateUrl: '../templates/components/form-field/form-field.html',
+            controller: 'formFieldCrtl',
+            scope: {
+                id: '@', 
+                placeholder: '@', 
+                icon : '@', 
+                label: '@'
+            }
+       } 
+    });
+    // .directive('', function() {
+
+    // });
